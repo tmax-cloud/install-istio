0090963bf8f4c57930b8caa57658483b602afdfb
MinjiYoo2 <minji_yoo@tmax.co.kr>
2022-06-24 10:28:57 +0900

    [chore] mod install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 sudo chmod +x version.conf
 source version.conf
 
-cp "$install_dir/yaml/instance.yaml" "$install_dir/yaml/instance_modified.yaml"
 cp "$install_dir/yaml/1.istio-base.yaml" "$install_dir/yaml/1.istio-base-modified.yaml"
 cp "$install_dir/yaml/jaeger-gatekeeper-forbidden-cm.yaml" "$install_dir/yaml/jaeger-gatekeeper-forbidden-cm-modified.yaml"
 cp "$install_dir/yaml/3.istio-core.yaml" "$install_dir/yaml/3.istio-core-modified.yaml"
 cp "$install_dir/yaml/4.istio-ingressgateway.yaml" "$install_dir/yaml/4.istio-ingressgateway-modified.yaml"
 cp "$install_dir/yaml/5.istio-metric.yaml" "$install_dir/yaml/5.istio-metric-modified.yaml"
 
+echo "---------- Storage type: ${STORAGE_NAME} ----------"
 if [ $STORAGE_NAME == "elasticsearch" ] ; then
     cp "$install_dir/yaml/2-1.istio-tracing-es.yaml" "$install_dir/yaml/2.istio-tracing-modified.yaml"
 else
@@ -18,7 +18,7 @@ else
     sed -i 's/{OPENSEARCH_UPWD}/'${OPENSEARCH_UPWD}'/g' "$install_dir/yaml/2.istio-tracing-modified.yaml"
 fi
 
-
+echo -e "\n---------- 폐쇄망 설정 ----------"
 sed -i 's/{CUSTOM_DOMAIN_NAME}/'${CUSTOM_DOMAIN_NAME}'/g' "$install_dir/yaml/2.istio-tracing-modified.yaml"
 sed -i 's/{JAEGER_VERSION}/'${JAEGER_VERSION}'/g' "$install_dir/yaml/2.istio-tracing-modified.yaml"
 sed -i 's/{GATEKEEPER_VERSION}/'${GATEKEEPER_VERSION}'/g' "$install_dir/yaml/2.istio-tracing-modified.yaml"
@@ -33,7 +33,7 @@ sed -i 's|{REDIRECT_URL}|'${REDIRECT_URL}'|g' "$install_dir/yaml/jaeger-gatekeep
 sed -i 's/{ISTIO_VERSION}/'${ISTIO_VERSION}'/g' "$install_dir/yaml/3.istio-core-modified.yaml"
 sed -i 's/{ISTIO_VERSION}/'${ISTIO_VERSION}'/g' "$install_dir/yaml/4.istio-ingressgateway-modified.yaml"
 
-
+echo -e "\n---------- Start Installation ----------"
 kubectl apply -f "$install_dir/yaml/1.istio-base-modified.yaml"
 kubectl apply -f "$install_dir/yaml/jaeger-gatekeeper-forbidden-cm-modified.yaml"
 kubectl apply -f "$install_dir/yaml/2.istio-tracing-modified.yaml"
